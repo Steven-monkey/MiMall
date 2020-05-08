ddd994f12f3f347a532c90d355b0e09e30b5fe89
monkey <445928814@qq.com>
2020-05-08 19:56:32 +0800

    storage封装

--- mall/src/App.vue
+++ mall/src/App.vue
@@ -5,12 +5,15 @@
 </template>
 
 <script>
+import storage from './storage/index'
 export default {
   name: 'App',
   components: {
   },
   mounted() {
-    
+    // storage.setItem('a',1)
+    // storage.setItem('user',{'a':1})
+    storage.setItem('abc',{'a':1},'user')
   },
 }
 </script>
